9d8ab1b1b4dadf4fb91e428e418bb918acd3a80c
Javier Cestau <javier.cestau1209@gmail.com>
2019-11-02 01:44:37 +0100

    final phase game

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour
+{
+    Transform playerTransform;
+
+    void Awake() {
+        playerTransform = GameObject.Find("Player").transform;
+    }
+
+   void Update() {
+         transform.position = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
+   }
+
+   void OnTriggerExit2D(Collider2D other)
+   {
+       if(other.CompareTag("Player")) {
+           playerTransform.gameObject.GetComponent<PlayerHealth>().GameOver();
+       }
+   }
+}
